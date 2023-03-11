043dea63d7493030364b6a7d2649816c8bb196a6
marcos <estudogit@outlook.com>
2023-03-11 01:14:41 -0300

    pagina adicionada ao app pages

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_bike_finder/app/ui/get_connect/get_connect_page.dart';
 import 'package:flutter_bike_finder/app/ui/home/home_page.dart';
 import 'package:flutter_bike_finder/app/ui/splash/splash_page.dart';
 import 'package:get/get.dart';
@@ -8,6 +9,7 @@ abstract class AppPages{
 
   static final pages = [
     GetPage(name: Routes.SPLASH, page: () => SplashPage()),
-    GetPage(name: Routes.HOME, page: () => HomePage())
+    GetPage(name: Routes.HOME, page: () => HomePage()),
+    GetPage(name: Routes.GETCONNECT, page: () => GetConnectPage()),
   ];
 }
