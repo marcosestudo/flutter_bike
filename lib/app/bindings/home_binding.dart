4bbbdf5ed32435213fa1e1a2f09de8f4a076d66b
marcos <estudogit@outlook.com>
2023-03-21 17:23:05 -0300

    binding

--- lib/app/bindings/home_binding.dart
+++ lib/app/bindings/home_binding.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_bike_finder/app/controllers/home_controller.dart';
+import 'package:flutter_bike_finder/app/data/providers/api.dart';
+import 'package:flutter_bike_finder/app/data/repository/map_repository.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class HomeBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(HomeController());
+    Get.put(HomeController(mapRepository: MapRepository(apiClient: BikeLocalizationApiClient(httpClient: http.Client()))));
   }
 }
--- lib/app/data/repository/map_repository.dart
+++ lib/app/data/repository/map_repository.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_bike_finder/app/data/providers/api.dart';
 import 'package:meta/meta.dart';
 
-class MyRepository {
+class MapRepository {
   final BikeLocalizationApiClient? apiClient;
 
-  MyRepository({@required this.apiClient}) : assert(apiClient != null);
+  MapRepository({@required this.apiClient}) : assert(apiClient != null);
 
   getAll() {
     return apiClient!.getAll();
